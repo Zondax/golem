59e3bfacc9c912c922ea6eac6433eec38282fb14
Alessio Treglia <alessio@jur.io>
2023-08-30 17:34:12 +0200

    add test case

--- pkg/utils/path_test.go
+++ pkg/utils/path_test.go
@@ -4,7 +4,33 @@ import (
 	"fmt"
 	"log"
 	"path/filepath"
+	"testing"
 )
+
+func TestExpandPath(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		want    string
+		wantErr bool
+	}{
+		{"empty", "", "", false},
+		{"non existing user", "~NONEXISTING/", "~NONEXISTING/", false},
+		{"any path", "/patgh/to/file", "/patgh/to/file", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ExpandPath(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExpandPath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ExpandPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func ExampleExpandPath() {
 	p, err := ExpandPath("~/test")
