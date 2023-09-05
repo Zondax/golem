372573a02234290c1703b90405a73a0721f2451c
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2023-09-05 12:22:09 +0200

    Add increment and decrement methods metrics

--- pkg/metrics/handler.go
+++ pkg/metrics/handler.go
@@ -1,8 +1,8 @@
 package metrics
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
-	"go.uber.org/zap"
 )
 
 type MetricHandler interface {
@@ -10,16 +10,48 @@ type MetricHandler interface {
 	Type() string
 }
 
-func (t *taskMetrics) UpdateMetric(name string, value float64, labels ...string) {
+type IncrementDecrementHandler interface {
+	MetricHandler
+	Inc(collector prometheus.Collector, labels ...string) error
+	Dec(collector prometheus.Collector, labels ...string) error
+}
+
+func (t *taskMetrics) performMetricAction(name string, action func(MetricHandler, prometheus.Collector, ...string) error, labels ...string) error {
 	t.mux.RLock()
 	metricDetail, ok := t.metrics[name]
 	t.mux.RUnlock()
+
 	if !ok {
-		zap.S().Errorf("Error: Metric not found for %s", name)
-		return
+		return fmt.Errorf("error: Metric not found for %s", name)
 	}
 
-	if err := metricDetail.Handler.Update(metricDetail.Collector, value, labels...); err != nil {
-		zap.S().Errorf("Error updating metric %s. Err: %s", name, err.Error())
+	if err := action(metricDetail.Handler, metricDetail.Collector, labels...); err != nil {
+		return fmt.Errorf("error performing action on metric %s. Err: %s", name, err.Error())
 	}
+
+	return nil
+}
+
+func (t *taskMetrics) UpdateMetric(name string, value float64, labels ...string) error {
+	return t.performMetricAction(name, func(h MetricHandler, c prometheus.Collector, labels ...string) error {
+		return h.Update(c, value, labels...)
+	}, labels...)
+}
+
+func (t *taskMetrics) IncrementMetric(name string, labels ...string) error {
+	return t.performMetricAction(name, func(h MetricHandler, c prometheus.Collector, labels ...string) error {
+		if incDecHandler, ok := h.(IncrementDecrementHandler); ok {
+			return incDecHandler.Inc(c, labels...)
+		}
+		return fmt.Errorf("error: Metric %s cannot be incremented", name)
+	}, labels...)
+}
+
+func (t *taskMetrics) DecrementMetric(name string, labels ...string) error {
+	return t.performMetricAction(name, func(h MetricHandler, c prometheus.Collector, labels ...string) error {
+		if incDecHandler, ok := h.(IncrementDecrementHandler); ok {
+			return incDecHandler.Dec(c, labels...)
+		}
+		return fmt.Errorf("error: Metric %s cannot be decremented", name)
+	}, labels...)
 }
