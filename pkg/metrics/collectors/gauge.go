64200040519856aa1373c91f99bb92a835e9a131
Lucas Francisco López <lucaslopezf@gmail.com>
2023-09-05 14:08:29 +0200

    Extends the existing MetricHandler interface to include Inc() and Dec() methods
    * Add increment and decrement methods metrics
    
    * Update pkg/metrics/readme

--- pkg/metrics/collectors/gauge.go
+++ pkg/metrics/collectors/gauge.go
@@ -24,3 +24,39 @@ func (g *Gauge) Update(collector prometheus.Collector, value float64, labels ...
 
 	return fmt.Errorf("invalid metric type, expected Gauge")
 }
+
+func (g *Gauge) Inc(collector prometheus.Collector, labels ...string) error {
+	if len(labels) > 0 {
+		if metricVec, ok := collector.(*prometheus.GaugeVec); ok {
+			metric := metricVec.WithLabelValues(labels...)
+			metric.Inc()
+			return nil
+		}
+		return fmt.Errorf("invalid metric type, expected GaugeVec for labels")
+	}
+
+	if metric, ok := collector.(prometheus.Gauge); ok {
+		metric.Inc()
+		return nil
+	}
+
+	return fmt.Errorf("invalid metric type, expected Gauge")
+}
+
+func (g *Gauge) Dec(collector prometheus.Collector, labels ...string) error {
+	if len(labels) > 0 {
+		if metricVec, ok := collector.(*prometheus.GaugeVec); ok {
+			metric := metricVec.WithLabelValues(labels...)
+			metric.Dec()
+			return nil
+		}
+		return fmt.Errorf("invalid metric type, expected GaugeVec for labels")
+	}
+
+	if metric, ok := collector.(prometheus.Gauge); ok {
+		metric.Dec()
+		return nil
+	}
+
+	return fmt.Errorf("invalid metric type, expected Gauge")
+}
--- pkg/metrics/handler_test.go
+++ pkg/metrics/handler_test.go
@@ -1,10 +1,25 @@
 package metrics
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
+
+type MetricsTestSuite struct {
+	suite.Suite
+	mockH *mockHandler
+}
+
+func (suite *MetricsTestSuite) SetupTest() {
+	suite.mockH = new(mockHandler)
+}
+
+func TestMetricsTestSuite(t *testing.T) {
+	suite.Run(t, new(MetricsTestSuite))
+}
 
 type mockHandler struct {
 	mock.Mock
@@ -15,31 +30,89 @@ func (m *mockHandler) Update(collector prometheus.Collector, value float64, labe
 	return args.Error(0)
 }
 
+func (m *mockHandler) Inc(collector prometheus.Collector, labels ...string) error {
+	args := m.Called(collector, labels)
+	return args.Error(0)
+}
+
+func (m *mockHandler) Dec(collector prometheus.Collector, labels ...string) error {
+	args := m.Called(collector, labels)
+	return args.Error(0)
+}
+
 func (m *mockHandler) Type() string {
 	args := m.Called()
 	return args.String(0)
 }
 
-func TestUpdateMetric(t *testing.T) {
-	mockH := new(mockHandler)
-
+func (suite *MetricsTestSuite) TestUpdateMetric() {
 	realCounter := prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "test_counter",
 		Help: "Test counter",
 	})
-
-	metricDetail := MetricDetail{
-		Collector: realCounter,
-		Handler:   mockH,
-	}
-
 	taskMetric := &taskMetrics{
 		metrics: make(map[string]MetricDetail),
 	}
-	taskMetric.metrics["test_metric"] = metricDetail
+	taskMetric.metrics["test_metric"] = MetricDetail{
+		Collector: realCounter,
+		Handler:   suite.mockH,
+	}
+	suite.mockH.On("Update", realCounter, 1.0, mock.Anything).Return(nil).Once()
+	err := taskMetric.UpdateMetric("test_metric", 1.0)
+	suite.Nil(err)
+	suite.mockH.AssertExpectations(suite.T())
+}
 
-	mockH.On("Update", realCounter, 1.0, mock.Anything).Return(nil).Once()
+func (suite *MetricsTestSuite) TestIncrementMetric() {
+	realGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "test_gauge",
+		Help: "Test gauge",
+	})
+	taskMetric := &taskMetrics{
+		metrics: make(map[string]MetricDetail),
+	}
+	taskMetric.metrics["test_metric"] = MetricDetail{
+		Collector: realGauge,
+		Handler:   suite.mockH,
+	}
+	suite.mockH.On("Inc", realGauge, mock.Anything).Return(nil).Once()
+	err := taskMetric.IncrementMetric("test_metric")
+	suite.Nil(err)
+	suite.mockH.AssertExpectations(suite.T())
+}
 
-	taskMetric.UpdateMetric("test_metric", 1.0)
-	mockH.AssertExpectations(t)
+func (suite *MetricsTestSuite) TestDecrementMetric() {
+	realGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "test_gauge",
+		Help: "Test gauge",
+	})
+	taskMetric := &taskMetrics{
+		metrics: make(map[string]MetricDetail),
+	}
+	taskMetric.metrics["test_metric"] = MetricDetail{
+		Collector: realGauge,
+		Handler:   suite.mockH,
+	}
+	suite.mockH.On("Dec", realGauge, mock.Anything).Return(nil).Once()
+	err := taskMetric.DecrementMetric("test_metric")
+	suite.Nil(err)
+	suite.mockH.AssertExpectations(suite.T())
+}
+
+func (suite *MetricsTestSuite) TestIncrementNonGaugeMetric() {
+	realCounter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "test_counter",
+		Help: "Test counter",
+	})
+	taskMetric := &taskMetrics{
+		metrics: make(map[string]MetricDetail),
+	}
+	taskMetric.metrics["test_metric"] = MetricDetail{
+		Collector: realCounter,
+		Handler:   suite.mockH,
+	}
+	suite.mockH.On("Inc", realCounter, mock.Anything).Return(fmt.Errorf("Error: Metric test_metric cannot be incremented")).Once()
+	err := taskMetric.IncrementMetric("test_metric")
+	suite.Error(err)
+	suite.mockH.AssertExpectations(suite.T())
 }
