04fd0e1f6fce9442b4a950df34002927908f855b
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2023-09-13 22:38:01 +0200

    Add level to logger

--- pkg/cli/logging.go
+++ pkg/cli/logging.go
@@ -6,13 +6,36 @@ import (
 	"os"
 )
 
-func InitGlobalLogger() *zap.Logger {
+const (
+	DebugLevel = "debug"
+	InfoLevel  = "info"
+	WarnLevel  = "warn"
+	ErrorLevel = "error"
+	FatalLevel = "fatal"
+	PanicLevel = "panic"
+)
+
+var stringToLevel = map[string]zapcore.Level{
+	DebugLevel: zapcore.DebugLevel,
+	InfoLevel:  zapcore.InfoLevel,
+	WarnLevel:  zapcore.WarnLevel,
+	ErrorLevel: zapcore.ErrorLevel,
+	FatalLevel: zapcore.FatalLevel,
+	PanicLevel: zapcore.PanicLevel,
+}
+
+func InitGlobalLogger(level string) *zap.Logger {
+	zapLevel, ok := stringToLevel[level]
+	if !ok {
+		zapLevel = zapcore.DebugLevel
+	}
+
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	encoder := zapcore.NewConsoleEncoder(encoderConfig)
 
-	core := zapcore.NewCore(encoder, os.Stdout, zapcore.DebugLevel)
+	core := zapcore.NewCore(encoder, os.Stdout, zapLevel)
 	logger := zap.New(core)
 
 	zap.ReplaceGlobals(logger)
