10e375cac234b02e0785937e3099ab72584d132e
Lucas Francisco López <lucaslopezf@gmail.com>
2023-11-10 13:49:11 +0100

    Add union methods to zdb (#24)
    * Add zdb methods
    
    * Tests
    
    * Add union distinct method to zdb

--- pkg/zdb/zdb.go
+++ pkg/zdb/zdb.go
@@ -24,6 +24,9 @@ type ZDatabase interface {
 	ScanRows(rows *sql.Rows, result interface{}) error
 	Select(query interface{}, args ...interface{}) ZDatabase
 	Where(query interface{}, args ...interface{}) ZDatabase
+	Joins(query string, args ...interface{}) ZDatabase
+	UnionAll(subQuery1 ZDatabase, subQuery2 ZDatabase) ZDatabase
+	UnionDistinct(subQuery1 ZDatabase, subQuery2 ZDatabase) ZDatabase
 	Limit(limit int) ZDatabase
 	Offset(offset int) ZDatabase
 	Order(value interface{}) ZDatabase
