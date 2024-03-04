4f869dae8a1bec808c5699bbd49d8b6443055b84
Lucas Francisco López <lucaslopezf@gmail.com>
2024-03-04 13:09:20 -0300

    Add method,path to activeConnections metric (#81)
    * Add method,path to activeConnection metric
    
    * fix ci
    
    * Minor fix

--- pkg/zrouter/zmiddlewares/metrics.go
+++ pkg/zrouter/zmiddlewares/metrics.go
@@ -3,6 +3,7 @@ package zmiddlewares
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/zondax/golem/pkg/logger"
 	"github.com/zondax/golem/pkg/metrics"
 	"github.com/zondax/golem/pkg/metrics/collectors"
 	"net/http"
@@ -37,7 +38,7 @@ func RegisterRequestMetrics(appName string, metricsServer metrics.TaskMetrics) [
 	register(totalRequestsMetricName, "Total number of HTTP requests made.", []string{"method", "path", "status"}, &collectors.Counter{})
 	register(durationMillisecondsMetricName, "Duration of HTTP requests in milliseconds.", []string{"method", "path", "status"}, &collectors.Histogram{})
 	register(responseSizeMetricName, "Size of HTTP response in bytes.", []string{"method", "path", "status"}, &collectors.Histogram{})
-	register(activeConnectionsMetricName, "Number of active HTTP connections.", nil, &collectors.Gauge{})
+	register(activeConnectionsMetricName, "Number of active HTTP connections.", []string{"method", "path"}, &collectors.Gauge{})
 
 	cacheHitsMetricName := getMetricName(appName, cacheHitsMetric)
 	cacheMissesMetricName := getMetricName(appName, cacheMissesMetric)
@@ -55,12 +56,15 @@ func RequestMetrics(appName string, metricsServer metrics.TaskMetrics) Middlewar
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			path := chi.RouteContext(r.Context()).RoutePattern()
 			startTime := time.Now()
 			activeConnectionsMetricName := getMetricName(appName, activeConnectionsMetricType)
 
 			mu.Lock()
 			activeConnections++
-			_ = metricsServer.UpdateMetric(activeConnectionsMetricName, float64(activeConnections))
+			if err := metricsServer.UpdateMetric(activeConnectionsMetricName, float64(activeConnections), r.Method, path); err != nil {
+				logger.GetLoggerFromContext(r.Context()).Errorf("error updating active connections metric: %v", err.Error())
+			}
 			mu.Unlock()
 
 			mrw := &responseWriter{ResponseWriter: w}
@@ -68,11 +72,12 @@ func RequestMetrics(appName string, metricsServer metrics.TaskMetrics) Middlewar
 
 			mu.Lock()
 			activeConnections--
-			_ = metricsServer.UpdateMetric(activeConnectionsMetricName, float64(activeConnections))
+			if err := metricsServer.UpdateMetric(activeConnectionsMetricName, float64(activeConnections), r.Method, path); err != nil {
+				logger.GetLoggerFromContext(r.Context()).Errorf("error updating active connections metric: %v", err.Error())
+			}
 			mu.Unlock()
 
 			duration := float64(time.Since(startTime).Milliseconds())
-			path := chi.RouteContext(r.Context()).RoutePattern()
 
 			responseStatus := mrw.status
 			bytesWritten := mrw.written
