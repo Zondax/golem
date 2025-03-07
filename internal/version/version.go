91202d243e71f8e35251f4719525ebc447a56a9e
harshitha maddipati <harshithamaddipati@harshitha-ka-MacBook-Air.local>
2025-03-07 16:13:58 +0530

    testing: Migration from bigcache to ristretto

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,7 +5,3 @@ import _ "embed"
 //go:generate bash get_version.sh
 //go:embed version.txt
 var GitVersion string
-
-//go:generate bash get_revision.sh
-//go:embed revision.txt
-var GitRevision string
