d209b6dbf9a0daadc2ccc6cc7e4bf13a31200f1a
Harshitha122 <71165978+Harshitha122@users.noreply.github.com>
2025-04-14 18:56:01 +0530

    Bigcache replacement (#187)
    * testing: Migration from bigcache to ristretto
    
    * testing: Migration from bigcache to ristretto
    
    * testing: Migration from bigcache to ristretto
    
    * testing: Migration from bigcache to ristretto
    
    * testing: Migration from bigcache to ristretto
    
    * testing: Migration from bigcache to ristretto
    
    * fixing: Migration from bigcache to ristretto
    
    * fixing version issues
    
    * resolving errors related to metrics initialisation
    
    * resolving errors related to metrics initialisation
    
    * resolving errors related to metrics initialisation
    
    * resolving errors related to metrics initialisation
    
    * Revert "resolving errors related to metrics initialisation"
    
    This reverts commit e7bcda273d54937613e3a83e11dda3e9a5c927c9.
    
    	modified:   pkg/zrouter/zmiddlewares/zcache_test.go
    
    * Revert "resolving errors related to metrics initialisation"
    
    This reverts commit 5a7e5683a8a87e2ed403eb26216c61e31086846b.
    
    	modified:   pkg/zrouter/zmiddlewares/common_test.go
    
    * Revert "resolving errors related to metrics initialisation"
    
    This reverts commit 9b9a221dba3d1e5334a668b8842431b174b6162b.
    
    	modified:   pkg/zrouter/zmiddlewares/zcache_test.go
    
    * updated tests
    
    * updated tests
    
    * Run go mod tidy to update dependencies
    
    * Run go mod tidy to update dependencies
    
    * Run go mod tidy to update dependencies
    
    * resolving comments
    
    * resolving comments
    
    * fixing
    
    * fixing
    
    * fixing
    
    * feat: use sync map
    
    * fix: lint
    
    * fixing cleanup process
    
    * fixing unused errors-lint
    
    * configuring parameters in locaclcache
    
    ---------
    
    Co-authored-by: harshitha maddipati <harshithamaddipati@harshitha-ka-MacBook-Air.local>
    Co-authored-by: Eric Mokaya <emagembe@gmail.com>

--- pkg/zcache/local_cache.go
+++ pkg/zcache/local_cache.go
@@ -4,30 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
 
+	"github.com/dgraph-io/ristretto"
 	"github.com/zondax/golem/pkg/logger"
-
-	"github.com/allegro/bigcache/v3"
 	"github.com/zondax/golem/pkg/metrics"
-	"github.com/zondax/golem/pkg/metrics/collectors"
 )
 
+//nolint:unused,varcheck,deadcode
 const (
 	neverExpires = -1
-
-	errorTypeLabel         = "error_type"
-	itemCountLabel         = "item_count"
-	residentItemCountLabel = "resident_item_count"
-	deletedItemCountLabel  = "deleted_item_count"
-	iterationErrorLabel    = "iteration_error"
-	unmarshalErrorLabel    = "unmarshal_error"
-	deletionErrorLabel     = "deletion_error"
-
-	cleanupItemCountMetricKey        = "local_cache_cleanup_item_count"
-	cleanupDeletedItemCountMetricKey = "local_cache_cleanup_deleted_item_count"
-	cleanupErrorMetricKey            = "local_cache_cleanup_errors"
-	cleanupLastRunMetricKey          = "local_cache_cleanup_last_run"
+	cacheCost    = 1
 )
 
 type CacheItem struct {
@@ -59,11 +47,10 @@ type LocalCache interface {
 }
 
 type localCache struct {
-	client         *bigcache.BigCache
-	prefix         string
-	logger         *logger.Logger
-	metricsServer  metrics.TaskMetrics
-	cleanupProcess CleanupProcess
+	client        *ristretto.Cache
+	prefix        string
+	logger        *logger.Logger
+	metricsServer metrics.TaskMetrics
 }
 
 func (c *localCache) Set(_ context.Context, key string, value interface{}, ttl time.Duration) error {
@@ -71,171 +58,58 @@ func (c *localCache) Set(_ context.Context, key string, value interface{}, ttl t
 
 	b, err := json.Marshal(value)
 	if err != nil {
-		c.logger.Errorf("error marshalling cache item value, key: [%s], err: [%s]", realKey, err)
+		c.logger.Errorf("error marshalling value, key: [%s], err: [%s]", realKey, err)
 		return err
 	}
 
-	cacheItem := NewCacheItem(b, ttl)
-	itemBytes, err := json.Marshal(cacheItem)
-	if err != nil {
-		c.logger.Errorf("error marshalling cache item, key: [%s], err: [%s]", realKey, err)
-		return err
+	// Handle never expires case
+	if ttl < 0 {
+		ttl = 0 // 0 means never expire in Ristretto
 	}
 
-	c.logger.Debugf("set key on local cache with TTL, key: [%s], value: [%v], ttl: [%v]", realKey, value, ttl)
-	if err = c.client.Set(realKey, itemBytes); err != nil {
-		c.logger.Errorf("error setting new key on local cache, fullKey: [%s], err: [%s]", realKey, err)
+	if !c.client.SetWithTTL(realKey, b, cacheCost, ttl) {
+		c.logger.Errorf("error setting new key on local cache, fullKey: [%s]", realKey)
+		return errors.New("failed to set key with TTL")
 	}
 
-	return err
+	c.client.Wait()
+	return nil
 }
 
 func (c *localCache) Get(_ context.Context, key string, data interface{}) error {
 	realKey := getKeyWithPrefix(c.prefix, key)
 
-	c.logger.Debugf("get key on local cache, fullKey: [%s]", realKey)
-
-	val, err := c.client.Get(realKey)
-	if err != nil {
-		if c.IsNotFoundError(err) {
-			c.logger.Debugf("key not found on local cache, fullKey: [%s]", realKey)
-		} else {
-			c.logger.Errorf("error getting key on local cache, fullKey: [%s], err: [%s]", realKey, err)
-		}
-
-		return err
+	val, found := c.client.Get(realKey)
+	if !found {
+		c.logger.Debugf("key not found on local cache, fullKey: [%s]", realKey)
+		return errors.New("cache miss")
 	}
 
-	var cachedItem CacheItem
-	if err := json.Unmarshal(val, &cachedItem); err != nil {
-		c.logger.Errorf("error unmarshalling cache item, key: [%s], err: [%s]", realKey, err)
-		return err
-	}
-
-	if cachedItem.IsExpired() {
-		c.logger.Debugf("key expired on local cache, key: [%s]", realKey)
-		_ = c.client.Delete(realKey)
-		return errors.New("cache item expired")
-	}
-
-	c.logger.Debugf("key retrieved from local cache, key: [%s]", realKey)
-	return json.Unmarshal(cachedItem.Value, data)
+	return json.Unmarshal(val.([]byte), data)
 }
 
-func (c *localCache) Delete(_ context.Context, key string) error {
+// Delete removes a value from the cache
+func (c *localCache) Delete(ctx context.Context, key string) error {
+	if c.client == nil {
+		return fmt.Errorf("cache client is not initialized")
+	}
 	realKey := getKeyWithPrefix(c.prefix, key)
 	c.logger.Debugf("delete key on local cache, fullKey: [%s]", realKey)
-
-	return c.client.Delete(realKey)
+	c.client.Del(realKey)
+	return nil
 }
 
 func (c *localCache) GetStats() ZCacheStats {
-	stats := c.client.Stats()
+	stats := c.client.Metrics
 	c.logger.Debugf("local cache stats: [%v]", stats)
 
-	return ZCacheStats{Local: &stats}
+	return ZCacheStats{Local: stats}
 }
 
 func (c *localCache) IsNotFoundError(err error) bool {
-	return errors.Is(err, bigcache.ErrEntryNotFound)
+	return err != nil && err.Error() == "cache miss"
 }
 
 func (c *localCache) setupAndMonitorMetrics(updateInterval time.Duration) {
 	setupAndMonitorCacheMetrics(c.metricsServer, c, c.logger, updateInterval)
-}
-
-func (c *localCache) startCleanupProcess() {
-	ticker := time.NewTicker(c.cleanupProcess.Interval)
-	go func() {
-		for range ticker.C {
-			c.cleanupExpiredKeys()
-		}
-	}()
-}
-
-func (c *localCache) cleanupExpiredKeys() {
-	iterator := c.client.Iterator()
-	var keysToDelete []string
-	var totalDeleted int
-	var totalResident int
-
-	for iterator.SetNext() {
-		entry, err := iterator.Value()
-		if err != nil {
-			c.logger.Errorf("[cleanup] - Error iterating over cache entries: %v", err)
-			if err = c.metricsServer.UpdateMetric(cleanupErrorMetricKey, 1, iterationErrorLabel); err != nil {
-				c.logger.Errorf("[cleanup] - error updating %s metric with label %s: [%s]", cleanupErrorMetricKey, iterationErrorLabel, err)
-			}
-			continue
-		}
-
-		var cachedItem CacheItem
-		if err = json.Unmarshal(entry.Value(), &cachedItem); err != nil {
-			c.logger.Errorf("[cleanup] - Error unmarshalling cache item: %v", err)
-			if err = c.metricsServer.UpdateMetric(cleanupErrorMetricKey, 1, unmarshalErrorLabel); err != nil {
-				c.logger.Errorf("[cleanup] - error updating %s metric with label %s: [%s]", cleanupErrorMetricKey, unmarshalErrorLabel, err)
-			}
-			continue
-		}
-
-		totalResident++
-
-		if cachedItem.IsExpired() {
-			keysToDelete = append(keysToDelete, entry.Key())
-		}
-
-		if len(keysToDelete) >= c.cleanupProcess.BatchSize {
-			totalDeleted += c.deleteKeysInBatch(keysToDelete)
-			keysToDelete = keysToDelete[:0]
-			time.Sleep(c.cleanupProcess.ThrottleTime)
-		}
-	}
-
-	if len(keysToDelete) > 0 {
-		totalDeleted += c.deleteKeysInBatch(keysToDelete)
-	}
-
-	// update metrics
-	if err := c.metricsServer.UpdateMetric(cleanupItemCountMetricKey, float64(totalResident-totalDeleted), residentItemCountLabel); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to update cleanup item count metric, err: %s", err)
-	}
-	if err := c.metricsServer.UpdateMetric(cleanupDeletedItemCountMetricKey, float64(totalDeleted), deletedItemCountLabel); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to update deletion cleanup deleted item count metric, err: %s", err)
-	}
-
-	if err := c.metricsServer.UpdateMetric(cleanupLastRunMetricKey, float64(time.Now().Unix())); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to update cleanup last run metric, err: %s", err)
-	}
-}
-
-func (c *localCache) deleteKeysInBatch(keys []string) (deleted int) {
-	for _, key := range keys {
-		if err := c.client.Delete(key); err != nil {
-			c.logger.Errorf("[cleanup] - Error deleting key %s: %v", key, err)
-			if err = c.metricsServer.UpdateMetric(cleanupErrorMetricKey, 1, deletionErrorLabel); err != nil {
-				c.logger.Errorf("[cleanup] - error updating %s metric with label %s: [%s]", cleanupErrorMetricKey, deletionErrorLabel, err)
-			}
-			continue
-		}
-		deleted++
-	}
-	return
-}
-
-func (c *localCache) registerCleanupMetrics() {
-	if err := c.metricsServer.RegisterMetric(cleanupErrorMetricKey, "Counts different types of errors occurred during cache cleanup process", []string{errorTypeLabel}, &collectors.Counter{}); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to register cleanup metrics, err: %s", err)
-	}
-
-	if err := c.metricsServer.RegisterMetric(cleanupItemCountMetricKey, "Counts the valid items in the cache during cache cleanup process", []string{itemCountLabel}, &collectors.Gauge{}); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to register cleanup metrics, err: %s", err)
-	}
-
-	if err := c.metricsServer.RegisterMetric(cleanupDeletedItemCountMetricKey, "Counts the expired (deleted) items in the cache during cache cleanup process", []string{itemCountLabel}, &collectors.Gauge{}); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to register cleanup metrics, err: %s", err)
-	}
-
-	if err := c.metricsServer.RegisterMetric(cleanupLastRunMetricKey, "Timestamp of the last cleanup process execution", []string{}, &collectors.Gauge{}); err != nil {
-		c.logger.Errorf("[cleanup] - Failed to register cleanup last run metric, err: %s", err)
-	}
 }
